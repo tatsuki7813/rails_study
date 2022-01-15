763fb75ecf91379a4cebb0621c5b4b75065ec50b
kobayashi.tatsuki <ttk7813@gmail.com>
2022-01-15 23:02:09 +0900

    membersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_01_15_133206) do
+
+  create_table "members", force: :cascade do |t|
+    t.integer "number", null: false
+    t.string "name", null: false
+    t.string "full_name"
+    t.string "email"
+    t.date "birthday"
+    t.integer "sex", default: 1, null: false
+    t.boolean "administrator", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
