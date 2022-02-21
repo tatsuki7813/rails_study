b3adfe532fcaf551295f48e4d20d41bc66d36703
kobayashi.tatsuki <ttk7813@gmail.com>
2022-02-21 22:42:35 +0900

    application.rbにi18nを設定

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Www
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = "Tokyo"
+    config.i18n.default_locale = :ja
     config.action_controller.permit_all_parameters = true
   end
 end
