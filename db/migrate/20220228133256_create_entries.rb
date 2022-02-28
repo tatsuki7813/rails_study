3ad61654f047cb82bf6db2eea9f25af0d8b8f5b3
kobayashi.tatsuki <ttk7813@gmail.com>
2022-02-28 22:48:20 +0900

    entriesテーブルを作成

--- db/migrate/20220228133256_create_entries.rb
+++ db/migrate/20220228133256_create_entries.rb
@@ -1,6 +1,11 @@
 class CreateEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :entries do |t|
+      t.references :member, null: false
+      t.string :title, null: false
+      t.text :body
+      t.datetime :posted_at, null: false
+      t.string :status, null: false, default: "draft"
 
       t.timestamps
     end
