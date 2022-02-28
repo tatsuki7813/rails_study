9f14591fee8e7cc2147a4b1109706f841621c58c
kobayashi.tatsuki <ttk7813@gmail.com>
2022-02-28 23:17:38 +0900

    Entryモデルのスコープを設定

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,4 +6,9 @@ class Entry < ApplicationRecord
   validates :title, presence: true, length: { maximum: 200 }
   validates :body, :posted_at, presence: true
   validates :status, inclusion: { in: STATUS_VALUES }
+
+  scope :common, -> { where(status: "public") }
+  scope :published, -> { where("status <> ?", "draft") }
+  scope :full, -> (member) { where("status <> ? OR member_id = ?", "draft", member.id ) }
+  scope :readable_for, -> (member) { member ? full(member) : common }
 end
