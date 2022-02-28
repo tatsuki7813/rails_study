4c1181d1623207fe28591e5fa00ddd78b0fc8003
kobayashi.tatsuki <ttk7813@gmail.com>
2022-02-28 23:06:52 +0900

    Entryのバリデーションを追加

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,9 @@
 class Entry < ApplicationRecord
   belongs_to :author, class_name: "Member", foreign_key: "member_id"
+
+  STATUS_VALUES = %w(draft member_only public)
+
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :body, :posted_at, presence: true
+  validates :status, inclusion: { in: STATUS_VALUES }
 end
