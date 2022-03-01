c5f755b73917fa5e280cb51fdbb7f1175523904f
kobayashi.tatsuki <ttk7813@gmail.com>
2022-03-01 22:51:53 +0900

    create entries index action and view

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,19 @@
 class EntriesController < ApplicationController
+  before_action :login_required, expect: [:index, :show]
+
   def index
+    if params[:member_id]
+      @member = Member.find(params[:member_id])
+      @entries = @member.entries
+    else
+      @entries = Entry.all
+    end
+
+    @entries = @entries.readable_for(current_member).order(posted_at: :desc)
   end
 
   def show
+    @entry = Entry.readable_for(current_member).find(params[:id])
   end
 
   def new
