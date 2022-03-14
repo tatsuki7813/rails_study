45d79e9af8c7aebb891403d969920e3547e105b7
kobayashi.tatsuki <ttk7813@gmail.com>
2022-03-14 22:52:32 +0900

    create articles show action and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.order(created_at: :desc)
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
